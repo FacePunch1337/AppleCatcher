22a539b04c32eca028f76c29d4d9d6c9f2c3fa76
FacePunch1337 <rezol1337@gmail.com>
2023-06-21 07:22:21 +0300

    save this sh*t

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -17,6 +17,9 @@ public class GameManager : MonoBehaviour
     private int score;
     private int bestScore;
 
+    private bool isGamePaused = false;
+    private bool isMenuOpen = false;
+    private bool hasGameStarted = false;
     private void Start()
     {
         hp = int.Parse(hpTxt.text);
@@ -27,20 +30,52 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (hpTxt.text == "0")
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            EndGame();
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else if (!isMenuOpen && hasGameStarted)
+            {
+                PauseGame();
+            }
         }
-        else if (hpTxt.text != "0")
+
+        if (!isGamePaused)
         {
-            hp--;
+            if (hpTxt.text == "0")
+            {
+                EndGame();
+            }
+            else if (hpTxt.text != "0")
+            {
+                hp--;
+            }
         }
     }
 
+    private void PauseGame()
+    {
+        isGamePaused = true;
+        Time.timeScale = 0f;
+        menuPanel.gameObject.SetActive(true);
+        restartButton.gameObject.GetComponentInChildren<TMP_Text>().text = "Restart";
+        isMenuOpen = true;
+    }
 
+    private void ResumeGame()
+    {
+        isGamePaused = false;
+        Time.timeScale = 1f;
+        menuPanel.gameObject.SetActive(false);
+        isMenuOpen = false;
+    }
 
     private void EndGame()
     {
+
+        PauseGame();
         menuPanel.gameObject.SetActive(true);
         restartButton.gameObject.SetActive(true);
         restartButton.gameObject.GetComponentInChildren<TMP_Text>().text = "Restart";
@@ -57,11 +92,27 @@ public class GameManager : MonoBehaviour
 
     public void PlayOrRestartGame()
     {
+        hasGameStarted = true;
+        ResumeGame();
         hpTxt.text = maxHPScore.ToString();
         scoreTxt.text = "0";
         menuPanel.gameObject.SetActive(false);
         appleSpawner.SetActive(true);
         box.SetActive(true);
+    }
+
+    public void OpenMenuPanel()
+    {
+        isMenuOpen = true;
+        menuPanel.gameObject.SetActive(true);
+        PauseGame();
+    }
+
+    public void CloseMenuPanel()
+    {
+        isMenuOpen = false;
+        menuPanel.gameObject.SetActive(false);
+        ResumeGame();
     }
 
     public void ExitGame()
